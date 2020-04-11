61c84767796aed6410d5183af533eceae8676129
Weston Mathews <westonmathews@gmail.com>
2020-04-11 15:42:57 -0700

    T Input Mapping and Applying Force
    Mapped keyboard controls and added force to tank via tank track controls

--- TankLords/Source/TankLords/TankTrack.cpp
+++ TankLords/Source/TankLords/TankTrack.cpp
@@ -5,8 +5,16 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-    // TODO Clamp throttle value so player can't overdrive
-
     auto Name = GetName();
     UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle);
+
+    // TODO Clamp throttle value so player can't overdrive
+    auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce; // FVector of force applied
+    auto ForceLocation = GetComponentLocation(); // FVector of track
+
+    // Get the actor in which force will be applied. Need to upcast to primitive component to apply physics
+    auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+    TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+
+
 }
--- TankLords/Source/TankLords/TankTrack.h
+++ TankLords/Source/TankLords/TankTrack.h
@@ -16,5 +16,9 @@ class TANKLORDS_API UTankTrack : public UStaticMeshComponent
 public:
 	UFUNCTION(BluePrintCallable, Category = Input)
 	void SetThrottle(float Throttle);
+
+	// Max force per track in newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; // Assume tank is 4000 kgs and 1g acceleration
 	
 };
